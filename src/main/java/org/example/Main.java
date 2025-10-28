a0d921859ee9c48e7b8d8977f40abeabb58a364d
Osman YILMAZ <osman.ylmz6126@gmail.com>
2025-10-28 23:49:44 +0300

    All functions are done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,61 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int number) {
+
+        number = Math.abs(number);
+        int originalNumber = number;
+        int reverse = 0;
+
+        while (number > 0) {
+            int lastDigit = number % 10;
+            reverse = (reverse * 10) + lastDigit;
+            number /= 10;
+        }
+
+        return reverse == originalNumber;
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+
+        int sum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum == number;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+
+        if (number == 0) {
+            return "Zero";
+        }
+
+        StringBuilder result = new StringBuilder();
+        String[] words = {
+                "Zero", "One", "Two", "Three", "Four",
+                "Five", "Six", "Seven", "Eight", "Nine"
+        };
+
+        String numStr = String.valueOf(number);
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = Character.getNumericValue(numStr.charAt(i));
+            result.append(words[digit]);
+            if (i < numStr.length() - 1) {
+                result.append(" ");
+            }
+        }
+
+        return result.toString();
+    }
 }
